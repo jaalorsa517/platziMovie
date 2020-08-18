46f888b39a2377fd6744a0786ceb86c35c874019
Jaime Ortiz <jaalorsa517@hotmail>
2020-08-17 21:25:56 -0500

    Se agrego la barra de navegacion secundaria y su diseño responsivo

--- scripts/events.js
+++ scripts/events.js
@@ -4,9 +4,16 @@ const small = window.matchMedia('screen and (max-width:800px)');
 
 
 const menuUser = document.querySelector('.account');
-const buttonUser = document.querySelector("#menu-user");
+const buttonUser = document.querySelector("#menu-user-rigth");
 
-
+function hideShow() {
+    if (menuUser.classList.contains('is-active')) {
+        menuUser.classList.remove('is-active');
+    }
+    else {
+        menuUser.classList.add('is-active');
+    }
+}
 // Validación menu-user
 function validationMenuUser(event) {
     if (event.matches) {
@@ -17,31 +24,16 @@ function validationMenuUser(event) {
     }
 }
 
-function hideShow() {
-    if (menuUser.classList.contains('is-active')) {
-        menuUser.classList.remove('is-active');
-    }
-    else {
-        menuUser.classList.add('is-active');
-    }
-}
 small.addListener(validationMenuUser);
 validationMenuUser(small);
 
 //validacion textFieldFind
-const miniSmall = window.matchMedia('screen and (min-width:320px) and (max-width:650px)');
+const miniSmall = window.matchMedia('screen and (min-width:200px) and (max-width:650px)');
 const findText = document.querySelector('#findText');
 const findIcon = document.querySelector('.find-icon');
 const title = document.querySelector('.title-container');
-
-function validationFind(event) {
-    if (event.matches) {
-        findIcon.addEventListener('click', onTextFind);
-    }
-    else {
-        findIcon.addEventListener('click', onTextFind);
-    }
-}
+const secondaryButton = document.querySelector('#menu-user-left');
+const menuSecond = document.querySelector('#secondary');
 
 function onTextFind() {
     if (findText.classList.contains('is-active')) {
@@ -51,6 +43,24 @@ function onTextFind() {
     else {
         findText.classList.add('is-active');
         title.classList.remove('is-active');
+    }
+}
+function onMenuSecond() {
+    if (menuSecond.style.left === "-100vw" || menuSecond.style.left === "") {
+        menuSecond.style.left = "0";
+    }
+    else {
+        menuSecond.style.left = "-100vw";
+    }
+}
+function validationFind(event) {
+    if (event.matches) {
+        findIcon.addEventListener('click', onTextFind);
+        secondaryButton.addEventListener('click', onMenuSecond)
+    }
+    else {
+        findIcon.removeEventListener('click', onTextFind);
+        secondaryButton.removeEventListener('click', onMenuSecond)
     }
 }
 
